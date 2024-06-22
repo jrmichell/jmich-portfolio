efd96abef576adf3c5778ec63029266b01298158
Jack Michell <me@jmich.dev>
2024-06-22 12:16:26 -0500

    update: page styling between sections

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24 py-24">
       <div className="space-y-24 my-10 md:mt-0 w-4/5 xl:w-3/5">
         <div className="my-16">
-          <div className="my-24 xl:my-56">
+          <div className="my-20 xl:my-56">
             <Hero />
           </div>
-          <div className="my-18 xl:my-56">
+          <div className="my-20 xl:my-56">
             <About />
           </div>
-          <div className="my-18 xl:my-56">
+          <div className="my-20 xl:my-56">
             <Projects />
           </div>
         </div>
