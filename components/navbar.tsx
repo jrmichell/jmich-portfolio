0ef8b16d9e45815a7141b2a9cd30381aa3c534e6
Jack Michell <me@jmich.dev>
2024-06-23 14:00:52 -0500

    update: started working on contact page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,14 +2,23 @@ import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/assets/logo.png";
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
     <div className="fixed z-50 bg-background w-full py-2">
-      <div className="flex justify-start items-center mx-8">
+      <div className="flex justify-start items-center mx-8 gap-6">
         <Link href="/">
           <Image src={Logo} alt="Logo" width={50} height={50} priority />
         </Link>
+        <Button variant="outline">
+          <Link href="/" className="uppercase">
+            Home
+          </Link>
+        </Button>
+        <Button variant="outline" className="text-muted-foreground uppercase">
+          Contact
+        </Button>
         <div className="fixed right-4">
           <ModeToggle />
         </div>
