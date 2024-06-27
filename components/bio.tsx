a2e92687bea482cf145b03862d1d646047322485
Jack Michell <me@jmich.dev>
2024-06-27 18:46:48 -0500

    update: Bio

--- components/bio.tsx
+++ components/bio.tsx
@@ -12,13 +12,12 @@ export default function Bio() {
         </CardHeader>
         <CardContent>
           <p className="text-muted-foreground">
-            I am a first year Computer Science major based in the United States.
-            I have a passion for creating different things and problem-solving.
-            Throughout my life, mathematics has always been something I was
-            fairly good with, and enjoyed the problem-solving aspect of it. I
-            have always been interested in technology and how it works, and I
-            want to take this passion and use it to create software that will
-            benefit the world.
+            I am software developer based in the United States. I have a passion
+            for creating different things and problem-solving. Throughout my
+            life, mathematics has always been something I was fairly good with,
+            and enjoyed the problem-solving aspect of it. I have always been
+            interested in technology and how it works, and I want to take this
+            passion and use it to create software that will benefit the world.
           </p>
         </CardContent>
       </Card>
