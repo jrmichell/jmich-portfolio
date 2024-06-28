632c8b43eb6828a7f1b7084530ec6e0901fa09af
Jack Michell <me@jmich.dev>
2024-06-28 08:51:36 -0500

    feat: Added tech stack portion to the about section (#7)
    * feat: Added tech stack portion to the about section
    
    * fix: PR #7

--- components/about.tsx
+++ components/about.tsx
@@ -2,6 +2,7 @@
 
 import { motion, AnimatePresence } from "framer-motion";
 import Bio from "./bio";
+import TechStack from "./tech-stack";
 
 export default function About() {
   return (
@@ -12,7 +13,13 @@ export default function About() {
         viewport={{ once: true }}
         transition={{ duration: 0.8 }}
       >
-        <Bio />
+        <h1 className="text-3xl mb-6 text-center font-semibold text-[#5651e5]">
+          About Me
+        </h1>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <Bio />
+          <TechStack />
+        </div>
       </motion.div>
     </AnimatePresence>
   );
--- components/bio.tsx
+++ components/bio.tsx
@@ -2,25 +2,20 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export default function Bio() {
   return (
-    <>
-      <h1 className="text-3xl mb-6 text-center font-semibold text-[#5651e5]">
-        About Me
-      </h1>
-      <Card className="text-center">
-        <CardHeader>
-          <CardTitle>Introduction</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-muted-foreground">
-            I am software developer based in the United States. I have a passion
-            for creating different things and problem-solving. Throughout my
-            life, mathematics has always been something I was fairly good with,
-            and enjoyed the problem-solving aspect of it. I have always been
-            interested in technology and how it works, and I want to take this
-            passion and use it to create software that will benefit the world.
-          </p>
-        </CardContent>
-      </Card>
-    </>
+    <Card className="text-center flex flex-col justify-center items-center">
+      <CardHeader>
+        <CardTitle>Introduction</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-muted-foreground">
+          I am a software engineer based in the United States. I have a passion
+          for creating different things and problem-solving. Throughout my life,
+          mathematics has always been something I was fairly good with, and
+          enjoyed the problem-solving aspect of it. I have always been
+          interested in technology and how it works, and I want to take this
+          passion and use it to create software that will benefit the world.
+        </p>
+      </CardContent>
+    </Card>
   );
 }
